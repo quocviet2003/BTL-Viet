755191e6973c6e6edc1b0783ba751422ab0120b6
quocviet2003 <quocvietkakasi2003@gmail.com>
2023-09-19 22:38:06 +0700

    init project

---
+++ API_BTL/Controllers/KhachController.cs
@@ -0,0 +1,97 @@
+﻿using DataModel;
+using BusinessLogicLayer;
+using Microsoft.AspNetCore.Mvc;
+namespace API_BTL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhachController : ControllerBase
+    {
+        private IKhachBusiness _khachBusiness;
+        public KhachController(IKhachBusiness khachBusiness)
+        {
+            _khachBusiness = khachBusiness;
+        }
+
+        [Route("get_all")]
+        [HttpGet]
+        public List<KhachModel> GetAll()
+        {
+            return _khachBusiness.GetAll();
+        }
+
+
+        [Route("get_by_id")]
+        [HttpGet]
+        public KhachModel GetDatabyID(string id)
+        {
+            return _khachBusiness.GetDatabyID(id);
+        }
+
+
+        [Route("Create")]
+        [HttpPost]
+        public KhachModel CreateItem([FromBody] KhachModel model) 
+        {
+            _khachBusiness.Create(model);
+            return model;
+        
+        }
+
+        [Route("Update")]
+        [HttpPost]
+        public KhachModel UpdateItem([FromBody] KhachModel model)
+        {
+            _khachBusiness.Update(model);
+            return model;
+        }
+
+        [Route("Delete")]
+        [HttpPost]
+        public IActionResult DeleteItem(string id)
+        {
+            bool result = _khachBusiness.Delete(id);
+            if (result)
+            {
+                return Ok("Xóa thành công");
+            }
+            else
+            {
+                return BadRequest("Không tìm thấy khách hàng hoặc xóa không thành công");
+            }
+        }
+
+
+
+        [Route("search")]
+        [HttpPost]
+        public IActionResult Search([FromBody] Dictionary<string, object> formData)
+        {
+            try
+            {
+                var page = int.Parse(formData["page"].ToString());
+                var pageSize = int.Parse(formData["pageSize"].ToString());
+                string ten_khach = "";
+                if (formData.Keys.Contains("ten_khach") && !string.IsNullOrEmpty(Convert.ToString(formData["ten_khach"]))) { ten_khach = Convert.ToString(formData["ten_khach"]); }
+                string dia_chi = "";
+                if (formData.Keys.Contains("dia_chi") && !string.IsNullOrEmpty(Convert.ToString(formData["dia_chi"]))) { dia_chi = Convert.ToString(formData["dia_chi"]); }
+                long total = 0;
+                var data = _khachBusiness.Search(page, pageSize, out total, ten_khach, dia_chi);
+                return Ok(
+                    new
+                    {
+                        TotalItems = total,
+                        Data = data,
+                        Page = page,
+                        PageSize = pageSize
+                    }
+                    );
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ API_BTL/Program.cs
@@ -0,0 +1,69 @@
+//using BusinessLogicLayer;
+//using DataAccessLayer;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddTransient<IDatabaseHelper, DatabaseHelper>();
+//builder.Services.AddTransient<IKhachBusiness, KhachBusiness>();
+//builder.Services.AddTransient<IKhachRepository, KhachRepository>();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+
+
+using BusinessLogicLayer;
+using DataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+// Add services to the container.
+builder.Services.AddTransient<IDatabaseHelper, DatabaseHelper>();
+builder.Services.AddTransient<IKhachRepository, KhachRepository>();
+builder.Services.AddTransient<IKhachBusiness, KhachBusiness>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseRouting();
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
